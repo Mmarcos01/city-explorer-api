b7ffc497bb464cb36a969d7389db67f2df11af5c
Marie <marie.marcos25@gmail.com>
2021-04-10 16:22:11 -0700

    server setup

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# city-explorer-api
+# City Explorer Server
+
+**Author**: Marie Marcos
+**Version**: 1.0.0
+
+## Overview
+A server created to house data for the App City Explorer.
+
+## Getting Started
+Have a properly set up machine
+Learn how to code
+JavaScript
+
+## Architecture
+Git, GitHub, JavaScript, Bootstrap, VScode, APIs, Developer Tools
+
+## Change Log
+04-010-2021 1:00pm - Server Setup
+04-010-2021 9:30pm - Locations Data
+
+
+## Credit and Collaborations
+Thanks Instructor Michelle Ferreirae
+
+## Feature Time
+Name of feature: Server Setup  
+Estimate of time needed to complete: 2hrs  
+Start time: 1300  
+Finish time: 1500  
+Actual time needed to complete:  2hrs  
+
+Name of feature: Weather (placeholder)  
+Estimate of time needed to complete: 2hrs  
+Start time: 1530  
+Finish time:
+Actual time needed to complete:   
---
+++ index.js
@@ -0,0 +1,30 @@
+//works like an import:
+const express = require('express');
+const cors =require('cors');
+//use .env file that was created:
+require('dotenv').config();
+//import weather:
+const weatherData = require('./data/weather.json');
+
+const app = express();
+//ensure data is accessible from the React frontend:s
+app.use (cors());
+//defines port and sets a backup default as 3001:
+const PORT = process.env.PORT || 3001;
+
+//<most of server definition goes here>
+
+//listening for GET requests at the path '/'
+app.get('/', (request, response) => {
+  response.send('hello');
+});
+
+//weather-data will be the path in the browser, used on the front-end
+app.get('/weather-data', (request, response) => {
+  response.json(weatherData);
+});
+
+//</most of server definition goes here />
+
+//setting up the server to listen will always be the last step in server files: (console.log callback for PoL)
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
