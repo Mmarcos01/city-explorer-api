51c4efa07a78ca388cf1ee56cf71076a18f96b91
Marie <marie.marcos25@gmail.com>
2021-04-10 19:49:50 -0700

    finished card 1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //works like an import:
 const express = require('express');
-const cors =require('cors');
+const cors = require('cors');
 //use .env file that was created:
 require('dotenv').config();
 //import weather:
@@ -8,7 +8,7 @@ const weatherData = require('./data/weather.json');
 
 const app = express();
 //ensure data is accessible from the React frontend:s
-app.use (cors());
+app.use(cors());
 //defines port and sets a backup default as 3001:
 const PORT = process.env.PORT || 3001;
 
@@ -20,9 +20,15 @@ app.get('/', (request, response) => {
 });
 
 //weather-data will be the path in the browser, used on the front-end
-app.get('/weather-data', (request, response) => {
-  response.json(weatherData);
+app.get('/weather', (request, response) => {
+  let forecastArr = weatherData.data.map(day => new DailyForecast(day));
+  response.json(forecastArr);
 });
+
+function DailyForecast(day) {
+  this.date = day.datetime;
+  this.description = day.weather.description;
+}
 
 //</most of server definition goes here />
 
