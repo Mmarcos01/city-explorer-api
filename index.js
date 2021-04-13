066d54da468c83e4b539db84fca08ad746c5e7fd
Marie <marie.marcos25@gmail.com>
2021-04-13 13:05:19 -0700

    completed backend tasks

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 //works like an import:
 const express = require('express');
 const cors = require('cors');
@@ -19,18 +20,31 @@ app.get('/', (request, response) => {
   response.send('hello');
 });
 
-//weather-data will be the path in the browser, used on the front-end
+// '/weather' will be the path in the browser, used on the front-end
+//forecastArr maps over weatherData, creating new instance of DailyForcast- showing properties of date and description
+
 app.get('/weather', (request, response) => {
-  let forecastArr = weatherData.data.map(day => new DailyForecast(day));
-  response.json(forecastArr);
+  try {
+    let forecastArr = weatherData.data.map(day => new DailyForecast(day));
+    response.json(forecastArr);
+  } catch (error) {
+    handleError(error, response);
+  }
 });
 
+
+//Create a constructor function for a `Forecast` object, that has a properties of `date` and `description`.
 function DailyForecast(day) {
   this.date = day.datetime;
   this.description = day.weather.description;
 }
 
+function handleError(error, response) {
+  response.status(500).send(' Status Code: 500 - Internal server error');
+}
+
 //</most of server definition goes here />
 
 //setting up the server to listen will always be the last step in server files: (console.log callback for PoL)
+//app.listen always takes 2 arguments: first argument is the port to listen on, second is a callback function to call when server is successfully listening
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
