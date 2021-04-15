77304048864090f7d25f2e27ea6bb53970fdaa31
Marie <marie.marcos25@gmail.com>
2021-04-14 23:32:00 -0700

    superagent constructor refactor

--- index.js
+++ index.js
@@ -5,46 +5,68 @@ const cors = require('cors');
 //use .env file that was created:
 require('dotenv').config();
 //import weather:
-const weatherData = require('./data/weather.json');
+
+// const weatherData = require('./data/weather.json');
 
 const app = express();
-//ensure data is accessible from the React frontend:s
-app.use(cors());
-//defines port and sets a backup default as 3001:
-const PORT = process.env.PORT || 3001;
 
-//<most of server definition goes here>
+app.use(cors());
+const PORT = process.env.PORT || 3001;
 
 //listening for GET requests at the path '/'
 app.get('/', (request, response) => {
   response.send('hello');
 });
 
-// '/weather' will be the path in the browser, used on the front-end
-//forecastArr maps over weatherData, creating new instance of DailyForcast- showing properties of date and description
+const superagent = require('superagent');
 
 app.get('/weather', (request, response) => {
-  try {
-    let forecastArr = weatherData.data.map(day => new DailyForecast(day));
-    response.json(forecastArr);
-  } catch (error) {
-    handleError(error, response);
-  }
+  superagent.get('https://api.weatherbit.io/v2.0/forecast/daily')
+    .query({
+      key: process.env.WEATHER_API_KEY,
+      units: 'I',
+      lat: request.query.lat,
+      lon: request.query.lon,
+    })
+    .then(weatherData => { 
+      response.json(weatherData.body.data.map(day => new DailyForecast(day)));
+    });
 });
 
+// app.get('/weather', (request, response) => {
+//   try {
+//     let forecastArr = weatherData.data.map(day => new DailyForecast(day));
+//     response.json(forecastArr);
+//   } catch (error) {
+//     handleError(error, response);
+//   }
+// });
 
-//Create a constructor function for a `Forecast` object, that has a properties of `date` and `description`.
+
 function DailyForecast(day) {
   this.date = day.datetime;
   this.description = day.weather.description;
 }
 
+// //get movies from api
+// app.get('/movies', getMovies);
+// //callback function, could be written within like get weather above
+// function getMovies(request, response) {
+//   const movieQuery = request.query.movieSearch;
+//   const url = 'https://api.themoviedb.org/3/movie/550?api_key=09da44311829b7b5aeabf8bcd0ca8042';
+//   const query = {
+//     client_id: process.env.MOVIE_API_KEY,
+//     query: movieQuery,
+//   }
+// }
+
 function handleError(error, response) {
   response.status(500).send(' Status Code: 500 - Internal server error');
 }
 
-//</most of server definition goes here />
-
-//setting up the server to listen will always be the last step in server files: (console.log callback for PoL)
-//app.listen always takes 2 arguments: first argument is the port to listen on, second is a callback function to call when server is successfully listening
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+
+
+
+
